b7dedea6e4e41a84db11e79de566218fec3fcf35
Ben Casalino <bencasalino@gmail.com>
2018-01-19 11:11:59 -0700

    submit

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+// Update with your config settings.
+module.exports = {
+  development: {
+    client: "pg",
+    connection: "postgres:///memoryv2"
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL
+  }
+};
